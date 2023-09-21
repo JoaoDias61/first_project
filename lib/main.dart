4c1a753b2a1554a268eb22305eeb53f76f7e10c0
JoaoDias61 <jpdias641@gmail.com>
2023-09-21 10:46:43 -0300

    added widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello test!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 45, 2, 121),
+              Color.fromARGB(255, 60, 18, 133),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello test!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
         ),
       ),
     ),
